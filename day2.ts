3e139f752ee71f8224085cd43d4c10839e3dd6ee
Kurzik, Jason (tue85656) <jason.kurzik@temple.edu>
2022-12-02 09:27:18 -0500

    Day 2 goal + input

---
+++ day2.ts
@@ -0,0 +1,19 @@
+import * as fs from 'node:fs/promises';
+
+async function main() {
+
+	//Rock, Paper Scissors tourny
+	//puzzle input is encrypted strategy guide
+	//column A is what opponent will play (A Rock, B, Paper, C Scissors)
+	//column B is what you should play (X,Y,Z)
+
+	//Score shape selected
+	// 1,2,3, RPS
+	// Outcome:
+	// 0 lost, 3 draw, 6 win
+
+	//Calculate score of strategy guide
+	
+}
+
+main();
